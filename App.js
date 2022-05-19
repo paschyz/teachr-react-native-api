848f7762726794fa09bd238f5efe704198f55d7b
paschyz <paschy2002@gmail.com>
2022-05-19 17:46:51 +0200

    aligned avatar with name

--- App.js
+++ App.js
@@ -2,7 +2,10 @@ import * as React from 'react';
 import {
   Text, 
   View,
-  SafeAreaView } from 'react-native';
+  SafeAreaView 
+} from 'react-native';
+import { Avatar } from 'react-native-paper';
+
 
 import Carousel from 'react-native-snap-carousel';
 
@@ -11,44 +14,60 @@ export default class App extends React.Component {
  
     constructor(props){
         super(props);
+        
         this.state = {
           activeIndex:0,
           carouselItems: [
           {
-              title:"Item 1",
-              text: "Text 1",
+            name:"Pierre du Parc de Locmania",
+            formation: "Université Paris Dauphine",
+            description: "Calme et patient, je sais m'adapter à l'élève,feznzjnfejnfzejnfenzeni ni nifzize n finz fezi"
           },
           {
-              title:"Item 2",
-              text: "Text 2",
+            name:"Name 2",
+            formation: "Formation 2",
+            description: "Description 5"
           },
           {
-              title:"Item 3",
-              text: "Text 3",
+            name:"Name 3",
+            formation: "Formation 3",
+            description: "Description 5"
           },
           {
-              title:"Item 4",
-              text: "Text 4",
+            name:"Name 4",
+            formation: "Formation 4",
+            description: "Description 5"
           },
           {
-              title:"Item 5",
-              text: "Text 5",
+            name:"Name 5",
+            formation: "Formation 5",
+            description: "Description 5"
           },
         ]
       }
     }
 
     _renderItem({item,index}){
+      const formationText="Formation"
+      const descriptionText = "Description";
         return (
           <View style={{
               backgroundColor:'floralwhite',
               borderRadius: 5,
-              height: 250,
-              padding: 50,
-              marginLeft: 25,
-              marginRight: 25, }}>
-            <Text style={{fontSize: 30}}>{item.title}</Text>
-            <Text>{item.text}</Text>
+              height: 400,
+              padding: 25,
+              marginLeft: 32,
+              marginRight: 0, }}>
+            <View style={{
+              flexDirection :'row',
+            }}>
+            <Avatar.Image size={50} source={require('C:/Users/d/Downloads/teachr/images/apple.jpg')} />
+            <Text style={{fontSize: 22, marginLeft: 15}}>{item.name}</Text>
+            </View>
+            <Text>{formationText}</Text>
+            <Text>{item.formation}</Text>
+            <Text>{descriptionText}</Text>
+            <Text>{item.description}</Text>
           </View>
 
         )
@@ -63,7 +82,7 @@ export default class App extends React.Component {
                   ref={ref => this.carousel = ref}
                   data={this.state.carouselItems}
                   sliderWidth={300}
-                  itemWidth={300}
+                  itemWidth={310}
                   renderItem={this._renderItem}
                   onSnapToItem = { index => this.setState({activeIndex:index}) } />
             </View>
