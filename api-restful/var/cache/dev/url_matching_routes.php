76dba034e755771a5794a91b378c861c04c67bb3
paschyz <paschy2002@gmail.com>
2022-05-20 14:58:50 +0200

    api page created

--- api-restful/var/cache/dev/url_matching_routes.php
+++ api-restful/var/cache/dev/url_matching_routes.php
@@ -11,11 +11,21 @@ return [
     ],
     [ // $regexpList
         0 => '{^(?'
-                .'|/_error/(\\d+)(?:\\.([^/]++))?(*:35)'
+                .'|/api(?'
+                    .'|(?:/(index)(?:\\.([^/]++))?)?(*:42)'
+                    .'|/(?'
+                        .'|docs(?:\\.([^/]++))?(*:72)'
+                        .'|contexts/(.+)(?:\\.([^/]++))?(*:107)'
+                    .')'
+                .')'
+                .'|/_error/(\\d+)(?:\\.([^/]++))?(*:145)'
             .')/?$}sDu',
     ],
     [ // $dynamicRoutes
-        35 => [
+        42 => [[['_route' => 'api_entrypoint', '_controller' => 'api_platform.action.entrypoint', '_format' => '', '_api_respond' => 'true', 'index' => 'index'], ['index', '_format'], null, null, false, true, null]],
+        72 => [[['_route' => 'api_doc', '_controller' => 'api_platform.action.documentation', '_format' => '', '_api_respond' => 'true'], ['_format'], null, null, false, true, null]],
+        107 => [[['_route' => 'api_jsonld_context', '_controller' => 'api_platform.jsonld.action.context', '_format' => 'jsonld', '_api_respond' => 'true'], ['shortName', '_format'], null, null, false, true, null]],
+        145 => [
             [['_route' => '_preview_error', '_controller' => 'error_controller::preview', '_format' => 'html'], ['code', '_format'], null, null, false, true, null],
             [null, null, null, null, false, false, 0],
         ],
