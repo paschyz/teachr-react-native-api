c02825bb114d2f61b26dbcbdbc93635158550169
paschyz <paschy2002@gmail.com>
2022-05-20 16:45:43 +0200

    http methods created (get, post, put)

--- api-restful/var/cache/dev/url_matching_routes.php
+++ api-restful/var/cache/dev/url_matching_routes.php
@@ -16,16 +16,27 @@ return [
                     .'|/(?'
                         .'|docs(?:\\.([^/]++))?(*:72)'
                         .'|contexts/(.+)(?:\\.([^/]++))?(*:107)'
+                        .'|teachrs(?'
+                            .'|(?:\\.([^/]++))?(?'
+                                .'|(*:143)'
+                            .')'
+                            .'|/([^/\\.]++)(?:\\.([^/]++))?(*:178)'
+                        .')'
                     .')'
                 .')'
-                .'|/_error/(\\d+)(?:\\.([^/]++))?(*:145)'
+                .'|/_error/(\\d+)(?:\\.([^/]++))?(*:217)'
             .')/?$}sDu',
     ],
     [ // $dynamicRoutes
         42 => [[['_route' => 'api_entrypoint', '_controller' => 'api_platform.action.entrypoint', '_format' => '', '_api_respond' => 'true', 'index' => 'index'], ['index', '_format'], null, null, false, true, null]],
         72 => [[['_route' => 'api_doc', '_controller' => 'api_platform.action.documentation', '_format' => '', '_api_respond' => 'true'], ['_format'], null, null, false, true, null]],
         107 => [[['_route' => 'api_jsonld_context', '_controller' => 'api_platform.jsonld.action.context', '_format' => 'jsonld', '_api_respond' => 'true'], ['shortName', '_format'], null, null, false, true, null]],
-        145 => [
+        143 => [
+            [['_route' => 'api_teachrs_get_collection', '_controller' => 'api_platform.action.get_collection', '_format' => null, '_stateless' => null, '_api_resource_class' => 'App\\Entity\\Teachr', '_api_identifiers' => ['id'], '_api_has_composite_identifier' => false, '_api_collection_operation_name' => 'get'], ['_format'], ['GET' => 0], null, false, true, null],
+            [['_route' => 'api_teachrs_post_collection', '_controller' => 'api_platform.action.post_collection', '_format' => null, '_stateless' => null, '_api_resource_class' => 'App\\Entity\\Teachr', '_api_identifiers' => ['id'], '_api_has_composite_identifier' => false, '_api_collection_operation_name' => 'post'], ['_format'], ['POST' => 0], null, false, true, null],
+        ],
+        178 => [[['_route' => 'api_teachrs_put_item', '_controller' => 'api_platform.action.put_item', '_format' => null, '_stateless' => null, '_api_resource_class' => 'App\\Entity\\Teachr', '_api_identifiers' => ['id'], '_api_has_composite_identifier' => false, '_api_item_operation_name' => 'put'], ['id', '_format'], ['PUT' => 0], null, false, true, null]],
+        217 => [
             [['_route' => '_preview_error', '_controller' => 'error_controller::preview', '_format' => 'html'], ['code', '_format'], null, null, false, true, null],
             [null, null, null, null, false, false, 0],
         ],
